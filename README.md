79d85ffe5b9787555bc45fea181a55e32d657987
The-Ivos <pixelartinautumn@gmail.com>
2024-09-11 13:02:21 +0200

    change was done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 this is the text of this file
+and here is another text
--- script_1.py
+++ script_1.py
@@ -1,1 +1,3 @@
-print('Hello World')
+print('Change was done!')
+print('Another change was done here!')
+print('doing some more changes')
