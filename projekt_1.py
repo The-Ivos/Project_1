9e91754290178db8f521f7398af9db34420eaf92
The-Ivos <pixelartinautumn@gmail.com>
2024-10-13 11:12:09 +0200

    changes

--- projekt_1.py
+++ projekt_1.py
@@ -37,7 +37,7 @@ garpike and stingray are also present.''',
 To ale pridat nejaky cisla. Jako treba 25. 98 nebo 539221''','''A tady
 je dalsi texttik IKLO Hoho
 ''','''A jeste jeden, jakoze 1. Ne 2!''','''2627 119 118 23, S50.''',
-'''Vcera jsem se dival na 37.dil M.A.S.H 4077, kde byl tank T50 a T30.Byl
+'''Vcera jsem se dival na 37.dil M.A.S.H, 4077, kde byl tank T50 a T30.Byl
 to vazne zajimavy dil. Videls ho taky?'''
 ]
 
@@ -105,11 +105,9 @@ while user:
                     splittedtext.remove(i)
                     for i in forsplitted:
                         splittedtext.append(i)
-                print(forsplitted)
-                print(addinstnt_list)
-                     
-
+                                 
             # TEXT IN MASH STYLE WITH NO PUNCT
+            print(splittedtext)
             nopunct_splitted = splittedtext.copy()
             for i in nopunct_splitted:
                 dotssum = []
@@ -119,13 +117,13 @@ while user:
                 if i.endswith(".") and len(dotssum) < 2:
                     splittedtext.remove(i)
                     splittedtext.append(i.replace(".",""))
-                if i.endswith(",") and len(dotssum) < 2:
+                if i.endswith(","):
                     splittedtext.remove(i)
                     splittedtext.append(i.replace(",",""))
-                if i.endswith("!") and len(dotssum) < 2:
+                if i.endswith("!"):
                     splittedtext.remove(i)
                     splittedtext.append(i.replace("!",""))
-                if i.endswith("?") and len(dotssum) < 2:
+                if i.endswith("?"):
                     splittedtext.remove(i)
                     splittedtext.append(i.replace("?",""))    
 
