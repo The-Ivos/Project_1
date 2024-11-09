434d5c553a79bf1f6101fd8365cb9a764730012c
The-Ivos <pixelartinautumn@gmail.com>
2024-11-09 22:37:13 +0100

    changes

--- Sandbox_81_chat.py
+++ Sandbox_81_chat.py
@@ -1,12 +1,51 @@
 import json
 import os
+import time
+
+def chatroom(x,y):
+    os.system("cls")
+
+    if "chat.txt" not in os.listdir():
+        chatfile = open("chat.txt","w",encoding="utf-8")
+        chatfile.write(f"BEGINNING OF THE CHAT HISTORY\n{time.ctime()}\n")
+        chatfile.close()
+
+    with open("chat.txt","a+",encoding="utf-8") as file:
+        file.seek(0,2)
+        file.write(f"\n\n{x} joined the chat on {time.ctime()}")
+        file.seek(0)
+                
+        while True:
+            file.seek(0)
+            content = file.read()
+            print(content)
+            print("\n\n" + str(50*"-"))
+            message = input(f"{x}: ")
+            if message.startswith("&"):
+                file.write(f"\n\n{x} tried some magic in VSC on {time.ctime()}")
+            
+            elif message == "":
+                message = "..."
+
+            elif message.lower() in ["quit","cancel","exit"] :
+                file.write(f"\n\n{x} left the chat on {time.ctime()}")
+                print("""You are leaving the chat!""")
+                input("Press any key to continue to main screen...")
+                break
+            else:
+                file.write(f"\n\n{x} [{time.ctime()}]:\n{message}")
+                os.system("cls")
+
+  
+    return main()
+
 
 def main():
     os.system("cls")
     print(f"""
-==============================
-WELCOME TO THE CHAT FOR LOSERS          
-==============================
+  ================================
+   WELCOME TO THE CHAT FOR LOSERS          
+  ================================
           
 Start with login or register new user:
           
@@ -14,7 +53,11 @@ Start with login or register new user:
           2) REGISTER NEW USER
 
           Q) QUIT
+          R) RESET CHAT
 
+---------------------------------------
+TIP: To exit in chat use "exit".
+---------------------------------------
 """)
     choice = input("Choose login(1),register new user(2) or (Q)uit:\n")
 
@@ -30,6 +73,25 @@ Start with login or register new user:
         os.system("cls")
         print("See ya next time!")
         exit()
+
+def login_pass(x):
+
+    password = input("password: ")
+
+    with open("users.json","r",encoding="utf-8") as file:
+        content = json.load(file)
+
+    if password.lower() == "cancel":
+        return main()
+    
+    while password != content[x]:
+        if password.lower() == "cancel":
+            return main()
+        print("Incorrect password!")
+        password = input("password: ")
+
+    return chatroom(x,password)
+
     
 def login():
     os.system("cls")
@@ -42,7 +104,7 @@ def login():
             return main()
 
         while username not in content:
-            new_reg = input(f"User not registered. Do you want to register new user {username}?\n")
+            new_reg = input(f"User not registered. Do you want to register new user {username}? (Yes/No)\n")
 
             while new_reg.lower() not in ("yes","no"):
                 new_reg = input("Please answer only 'yes' or 'no':\n")
@@ -51,10 +113,10 @@ def login():
                 return register_user(username)
             else:
                 return main()
+    
+    return login_pass(username)
 
 
-
-        print(content)
 
 def register_user(x):
     with open("users.json","r",encoding="utf-8") as users_file:
